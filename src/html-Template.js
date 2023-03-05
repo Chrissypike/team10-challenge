db73106760aad57c7b452ec2c3c6eb4cc3e89a44
Chrissypike <you@example.com>
2023-03-05 12:00:16 -0600

    sections

--- src/html-Template.js
+++ src/html-Template.js
@@ -11,7 +11,7 @@ module.exports = (profiles) => {
       <link rel="stylesheet" href="style.css">
   </head>
   <body>
-      <nav class="container1">
+      <nav class="navContainer">
         <div class="teamHeading">
             <div class="textCenter">The Team</div>
         </div>
@@ -29,16 +29,16 @@ const createProfiles = (profiles) => {
     // create the manager's html
     const createManager = (manager) => {
       return `
-      <section class="staffMemberBox">
+      <section class="employeeBox">
           <div class="boxHeader">
               <h2 class="boxTitle">${manager.getName()}</h2>
               <h3 class="boxTitle"></i>${manager.getRole()}</h3>
           </div>
           <div class="boxBody">
-              <ul class="listSet">
-                  <li class="listPersonItem">ID: ${manager.getId()}</li>
-                  <li class="listPersonItem">Email: <a href="Email to:${manager.getEmail()}">${manager.getEmail()}</a></li>
-                  <li class="listPersonItem">Office number: ${manager.getOfficeNumber()}</li>
+              <ul class="employeeList">
+                  <li class="employeeItem">ID: ${manager.getId()}</li>
+                  <li class="employeeItem">Email: <a href="Email to:${manager.getEmail()}">${manager.getEmail()}</a></li>
+                  <li class="employeeItem">Office number: ${manager.getOfficeNumber()}</li>
               </ul>
           </div>
       </section>
@@ -48,16 +48,16 @@ const createProfiles = (profiles) => {
     // create the engineer's html
     const createEngineer = (engineer) => {
       return `
-          <section class="staffMemberBox">
+          <section class="employeeBox">
       <div class="boxHeader">
           <h2 class="boxTitle">${engineer.getName()}</h2>
           <h3 class="boxTitle"></i>${engineer.getRole()}</h3>
       </div>
       <div class="boxBody">
-          <ul class="listSet">
-              <li class="listPersonItem ">ID: ${engineer.getId()}</li>
-              <li class="listPersonItem ">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
-              <li class="listPersonItem ">GitHub: <a href="https://github.com/${engineer.getUsername()}">${engineer.getUsername()}</a></li>
+          <ul class="employeeList">
+              <li class="employeeItem ">ID: ${engineer.getId()}</li>
+              <li class="employeeItem ">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+              <li class="employeeItem ">GitHub: <a href="https://github.com/${engineer.getUsername()}">${engineer.getUsername()}</a></li>
           </ul>
       </div> 
   </section>
@@ -67,16 +67,16 @@ const createProfiles = (profiles) => {
     // create intern's html
     const createIntern = (intern) => {
       return `
-          <section class="staffMemberBox">
+          <section class="employeeBox">
       <div class="boxHeader">
           <h2 class="boxTitle">${intern.getName()}</h2>
           <h3 class="boxTitle"></i>${intern.getRole()}</h3>
       </div>
       <div class="boxBody">
-          <ul class="listSet">
-              <li class="listPersonItem ">ID: ${intern.getId()}</li>
-              <li class="listPersonItem ">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
-              <li class="listPersonItem ">Education: ${intern.getUniversity()}</li>
+          <ul class="employeeList">
+              <li class="employeeItem ">ID: ${intern.getId()}</li>
+              <li class="employeeItem ">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+              <li class="employeeItem ">Education: ${intern.getUniversity()}</li>
           </ul>
       </div>
   </section>
